c96ea46f1e400e73036453e7c7b59cc351be3ac0
Felipe Paz <haohmaru@pop-os.localdomain>
2021-12-04 10:26:23 -0300

    feat: initial commit

---
+++ __tests__/test.spec.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const app = require('../index');
+
+describe('GET /', () => {
+
+  afterAll(() => {
+    app.close();
+  });
+
+  it('responds with Hello World', done => {
+    request(app)
+      .get('/')
+      .expect(200, 'Hello World!', done);
+  });
+});
---
+++ index.js
@@ -0,0 +1,16 @@
+const app = require('express')();
+const PORT = process.env.PORT || 3000;
+const http = require('http');
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+module.exports = server;
